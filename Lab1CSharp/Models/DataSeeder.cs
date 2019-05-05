a81e35e8b87d0d4d9ac8e1229579617842d28c11
Alexandru <dan.alexandru93@yahoo.com>
2019-05-05 10:21:42 +0300

    Lab1

---
+++ Lab1CSharp/Models/DataSeeder.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1CSharp.Models
+{
+    public class DataSeeder
+    {
+        public static void Initialize(IntroDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any movies.
+            if (context.Movies.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Movies.AddRange(
+                new Movie
+                {
+                    Title = "Titanic",
+                    Runtime = 170,
+                    Rating = 8.9,
+                    Storyline = "Love Story"
+                },
+                new Movie
+                {
+                    Title = "Skyfall",
+                    Runtime = 190,
+                    Rating = 9.2,
+                    Storyline = "Action"
+                });
+            context.SaveChanges();
+        }
+
+    }
+}
---
+++ Lab1CSharp/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1CSharp
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Title must have between 2 and 100 characters!")]
+        public string Title { get; set; }
+        public int Runtime { get; set; }
+        public double Rating { get; set; }
+        public string Storyline { get; set; }
+    }
+}
